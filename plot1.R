ba6de350939654d2a6cdfbaec6bb0816ea553526
jipatsaa <txibi72@Mac-mini-de-Aitziber-Atutxa.local>
2015-01-25 16:54:10 +0100

    adding triangles on the plot

--- plot1.R
+++ plot1.R
@@ -1,13 +1,57 @@
-source("downloadFromURLAndUnzip.R")
+library(plyr)
+
+#function to download and unzip the data
+downloadFromURLAndUnzip <- function  (fileUrl="http://dir.csv", workdirPath=".", fileName="dataFile")
+  # PRE: 3 parameters:
+  # 1. url to download
+  # 2. the target directory to dump the information
+  # 3. the name of the zip file 
+  
+  # POST: The file will be downloaded and unzip in the targetdirectory (creating the last one if it does not exist)  
+{
+  fileUnzip<-sub(".zip","",fileName)
+  fileAndPath=paste(workdirPath,fileUnzip,sep="/")
+  
+  print(fileAndPath)
+  
+  ## Step 0: Checking existence of file
+  
+  if(!file.exists(fileAndPath)){
+    ## Step 1: Checking dir and creating if needed
+    if(!file.exists(workdirPath))
+    {
+      dir.create(workdirPath)
+    }
+    
+    ## Step 2: download the data file
+    
+    if(!file.exists(fileAndPath))
+    {
+      download.file(fileUrl, destfile=fileAndPath,method="curl")
+      unzip(zipfile=fileAndPath, exdir="./data") 
+      dateDownloaded <- date()
+      print(paste("INFO: the data file downloaded on: ", dateDownloaded))
+    }
+  }
+}
+
+#calling the function
 downloadFromURLAndUnzip("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip","data","exdata_data_NEI_data")
 
 ## This first line will likely take a few seconds. Be patient!
 NEI <- readRDS("./data/summarySCC_PM25.rds")
-SCC <- readRDS("./data/Source_Classification_Code.rds")
-SCCSubset<-subset(SCC,select=c(SCC,Short.Name))
-data<-merge(SCCSubset,NEI,by="SCC")
-dataSumEmissionsByYear <- ddply(data, .(year), summarise, sumEmissions = sum(Emissions, na.rm = TRUE))
-plot(dataSumEmissionsByYear$year,dataSumEmissionsByYear$sumEmissions,type="l",xlab="Year",ylab="PM25-PRI Emissions",main="Emissions by Year")
+
+## Keep only the information needed
+NEISubset<-subset(NEI,select=c(year,Emissions)) 
+
+## Sum up the emissions by year
+NEISumEmissionsByYear <- ddply(NEISubset, .(year), summarise, sumEmissions = sum(Emissions, na.rm = TRUE))
+
+## Using the base plotting system to plot the evolution of the Emissions year by year
+## The plot shows that the Emissions decend
+par(mar=c(4,4,3,1))
+plot(NEISumEmissionsByYear$year,NEISumEmissionsByYear$sumEmissions,type="l",xlab="Year",ylab="PM25-PRI Emissions",main="Emissions by Year")
+points(NEISumEmissionsByYear$year,NEISumEmissionsByYear$sumEmissions,pch=2)#adding triangles showing the "real data points"
 pngFile <- "plot1.png"
-dev.copy(png, file = pngFile,  bg = "white")
+dev.copy(png, file = pngFile)
 dev.off()
